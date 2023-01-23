c13878f1d72451b8a12dd8b4fdce205351d4acfe
samico <loouiz@gmail.com>
2023-01-23 15:53:12 -0300

    Functional programming project

---
+++ functional_programming/script.py
@@ -0,0 +1,31 @@
+import csv
+from functools import reduce
+
+def count(predicate, itr):
+  count_filter = filter(lambda x: predicate, itr)
+  count_reduce = reduce(lambda x,y: x+1, count_filter, 0)
+  return count_reduce
+
+
+def average(itr):
+  # If itr is not iterable, this will generate an iterator.  
+  iterable = iter(itr) 
+  def avg_helper(curr_count, itr, curr_sum):
+    next_num = next(itr, "null")
+    if next_num == "null":
+        return curr_sum/curr_count
+    curr_count += 1
+    curr_sum += next_num
+    return avg_helper(curr_count, itr, curr_sum)
+  return avg_helper(0, iterable, 0)
+
+
+
+with open('advanced_python/functional_programming/1kSalesRec.csv', newline = '') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+  fields = next(reader)
+  count_belgiums = count(lambda x: x[1] == "Belgium", reader)
+  print(count_belgiums)
+  csvfile.seek(0)
+  avg_portugal = average(map(lambda x: float(x[13]), filter(lambda x: x[1] == "Portugal", reader)))
+  print(avg_portugal)
